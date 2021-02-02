307fdcc7c20593ac2f0e193ab296ddb530492083
T-shikai <tatsuroshikai@gmail.com>
2021-02-02 01:56:39 +0000

    Favorite And Comment

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,13 @@ class FavoritesController < ApplicationController
   def create
     @favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
     @favorite.save
-    redirect_to book_path(params[:book_id])
+    redirect_to request.referer
   end
 
   def destroy
     @favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
     @favorite.delete
-    redirect_to book_path(params[:book_id])
+    redirect_to request.referer
   end
 
 end
